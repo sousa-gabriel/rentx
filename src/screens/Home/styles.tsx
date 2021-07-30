9905da4ac56a058959f9dabceddd67890da8cfb7
sousa-gabriel <sousa-gabriel@hotmail.com>
2021-07-30 08:54:37 -0300

    configuring the svg and creating the Header

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,13 +1,28 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
+
+    background-color: ${({theme})=>theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    height: 113px;
+    justify-content: flex-end;
+    padding: 32px 24px;
+    background-color: ${({theme})=>theme.colors.header};
+`;
+
+export const HeaderContent = styled.View`
+    justify-content: space-between;
+    flex-direction: row;
     align-items: center;
 `;
 
-export const Title = styled.Text`
-    color: ${({theme})=> theme.colors.header};
-    font-size: 50px;
-    font-family: ${({theme})=> theme.fonts.secondary_600};
+export const TotalCars = styled.Text`
+    font-size: ${RFValue(15)}px;
+    font-family: ${({theme})=> theme.fonts.primary_400};
+    color: ${({theme})=> theme.colors.text};
 `;
